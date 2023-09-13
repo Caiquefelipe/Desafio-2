f87db09c7819a6b86229a79ca4f28d22e67b0ef7
Caiquefelipe <137807695+Caiquefelipe@users.noreply.github.com>
2023-09-13 09:58:42 -0300

    Criando uma class calculadora e enumerador

---
+++ script.js
@@ -0,0 +1,33 @@
+// Enumerador para as operações
+const OperacaoEnum = {
+    ADICAO: 'ADICAO',
+    SUBTRACAO: 'SUBTRACAO',
+    MULTIPLICACAO: 'MULTIPLICACAO',
+    DIVISAO: 'DIVISAO',
+  };
+  
+  class Calculadora {
+    constructor() {
+      this.valorA = '';
+      this.valorB = '';
+      this.operacao = '';
+      this.display = document.getElementById('display');
+    }
+  
+  }
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+  
+  
+ 
