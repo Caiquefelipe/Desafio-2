a45c9055de978d98f6a718d2d446b408720242ae
caiquesouza3736 <137809018+caiquesouza3736@users.noreply.github.com>
2023-09-27 08:01:35 -0300

    centralizando a calculadora

--- script.js
+++ script.js
@@ -1,62 +1,144 @@
-const Enumerador = {
-  ADICAO: '+',
-  MULTIPLICACAO: '*',
-  SUBTRACAO: '-',
-  DIVISAO: '/'
-}
-
-class Calculadora {
-
-  constructor(valorA, operacao, valorB, display) {
-    this._valorA = valorA;
-    this._operacao = operacao;
-    this._valorB = valorB;
-    this._display = document.getElementById('display');
-
-  }
-
-  executarCalculo (){
-    this._valorA = parseFloat(this._valorA);
-    this._valorB = parseFloat(this._valorB)
-    switch (this._operacao) {
-      case Enumerador.ADICAO:
-        return this._valorA +  this._valorB
-      case Enumerador.SUBTRACAO:
-        return  this._valorA -  this._valorB;
-      case Enumerador.MULTIPLICACAO:
-        return  this._valorA *  this._valorB;
-      case Enumerador.DIVISAO:
-       if(valorB === 0){
-        return "Erro:Divisao por zero"
-       }
-       return valorA / valorB;
-       default:
-        
-    }
-    
-  }
-  setOperacao(operacao) {
-    this.operacao = operacao;
-    console.log(` ${operacao}`)
-
-  }
-}
-
-
-const botao = document.querySelectorAll('.botao')
-const display = document.getElementById('display')
-const calcularBotao = document.querySelector('.calcular')
-botao.forEach((buttons) => {
-  buttons.addEventListener('click', () => {
-    if(buttons.innerHTML === Enumerador.ADICAO || buttons.SUBTRACAO || Enumerador.DIVISAO || Enumerador.MULTIPLICACAO)
-    display.innerHTML += buttons.innerHTML
-    
-  })
+const operacaoEnum = ({
+  Divisao: '/',
+  Multiplicacao: '*',
+  Adicao: '+',
+  Subtracao: '-'
 })
 
- 
+const operacoes = ['+', '-', '*', '/'];
+class Calculadora {
+  constructor() {
+    this._valorA = '';
+    this._valorB = '';
+    this._operacao = '';
+    this._resultado = '0.0';
+    this._display = '';
+  }
+  get valorA() {
+    return this._valorA;
+  }
 
-  const calculadora = new Calculadora(10, '+', 10)
-  console.log(calculadora.executarCalculo())
-  document.querySelector('.botaoLimpar').addEventListener("click", () => { display.innerHTML = "" })
+  get valorB() {
+    return this._valorB;
+  }
 
+  get operacao() {
+    return this._operacao;
+  }
+
+  get resultado() {
+    return this._resultado;
+  }
+
+  get display() {
+    return this._display;
+  }
+
+  set valorA(valor) {
+    this._valorA = valor;
+  }
+
+  set valorB(valor) {
+    this._valorB = valor;
+  }
+
+  set operacao(operacao) {
+    this._operacao = operacao;
+  }
+
+  set resultado(resultado) {
+    this._resultado = resultado;
+  }
+
+  set display(display) {
+    this._display = display;
+  }
+
+  adicionarDigito(digito) {
+    if (operacoes.includes(digito)) {
+      if (!this.operacao) {
+        this.operacao = digito;
+        this.display += ` ${this.operacao} `;
+        console.log(`operacao definida como: ${this.operacao}`)
+        return
+        
+      }
+    } else {
+      if (!this.operacao) {
+        this.valorA += digito;
+        console.log(`valor de valorA atual: ${this.valorA}`)
+      } else {
+        this.valorB += digito;
+      }
+      this.display += digito;
+      console.log(`valor de valorB atual:${this.valorB}`)
+    }
+   
+  }
+
+  
+
+  calcular() {
+    if (this.valorA && this.valorB && this.operacao) {
+      const numeroA = parseFloat(this.valorA.replace(',', '.'));
+      const numeroB = parseFloat(this.valorB.replace(',', '.'));
+
+      switch (this.operacao) {
+        case operacaoEnum.Adicao:
+          this.resultado = (numeroA + numeroB).toString().replace('.', ',');
+          break;
+        case operacaoEnum.Subtracao:
+          this.resultado = (numeroA - numeroB).toString().replace('.', ',');
+          break;
+        case operacaoEnum.Multiplicacao:
+          this.resultado = (numeroA * numeroB).toString().replace('.', ',');
+          break;
+        case operacaoEnum.Divisao:
+          if (numeroB === 0) {
+            this.resultado = 'Nao é possível dividir por zero';
+          } else {
+            this.resultado = (numeroA / numeroB).toString().replace('.', ',');
+            break
+          }
+
+      }
+
+
+      this.display = this.resultado;
+      this.valorA = '';
+      this.valorB = '';
+      this.operacao = '';
+    }
+  }
+
+  limparDisplay() {
+    this.valorA = '';
+    this.valorB = '';
+    this.operacao = '';
+    this.resultado = '0.0';
+    this.display = '';
+  }
+}
+
+const calculadora = new Calculadora();
+
+
+atribuirValor = (digito) => {
+  calculadora.adicionarDigito(digito);
+  atualizarDisplay();
+
+}
+
+calcular = () => {
+  calculadora.calcular();
+  atualizarDisplay();
+}
+
+atualizarDisplay = () => {
+  document.getElementById('display').value = calculadora.display;
+}
+
+limparDisplay = () => {
+  calculadora.limparDisplay();
+  atualizarDisplay();
+}
